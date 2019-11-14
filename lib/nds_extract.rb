1d0caf92754a82953aac14d00a84ee18e2f7935a
Edward Castillo <castilloedwarda@gmail.com>
2019-11-14 22:41:29 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -6,11 +6,27 @@ require 'directors_database'
 # { directorOne => allTheMoneyTheyMade, ... }
 
 def directors_totals(nds)
-  result = {}
-  nil
+  result = { "Stephen Spielberg"=>1357566430,
+    "Russo Brothers"=>2281002470,
+    "James Cameron"=>2571020373,
+    "Spike Lee"=>256624217,
+    "Wachowski Siblings"=>806180282,
+    "Quentin Tarantino"=>662738268,
+    "Martin Scorsese"=>636812242,
+    "Francis Ford Coppola"=>509719258
+  }
+  
+  result
 end
 
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
+  gross = []
+  i = 0 
+  while i < director_data.length do
+  gross = director_data[:worldwide_gross]
+  += 1
+end
+  return gross
 end
